f520f70b967a1e5eb446f0d12fd5548a4ce74ce0
Nicholas G <nlg@ucsd.edu>
2015-11-14 12:27:36 -0800

    fixed timer bug

--- js/notifications.js
+++ js/notifications.js
@@ -1,29 +1,37 @@
 // from Mozilla Dev Network
 // modified for modular use in our app
 function pushNotify(strToDisplay) {
-  // Check if the browser supports notifications
-  if (!("Notification" in window)) {
-    alert("This browser does not support desktop notification");
-  }
-
-  // Check whether notification permissions have already been granted
-  else if (Notification.permission === "granted") {
-    // If it's okay, create a notification
-    var notification = new Notification(strToDisplay);
-  }
-
-  // Otherwise, ask the user for permission
-  else if (Notification.permission !== 'denied') {
-    Notification.requestPermission(function (permission) {
-      // If the user accepts, create a notification
-      if (permission === "granted") {
+    if (Notification.permission === "granted") {
+        // If it's okay, create a notification
         var notification = new Notification(strToDisplay);
-      }
-    });
-  }
+    }
+
+    // Otherwise, ask the user for permission
+    else if (Notification.permission !== 'denied') {
+        Notification.requestPermission(function (permission) {
+            // If the user accepts, create a notification
+            if (permission === "granted") {
+                var notification = new Notification(strToDisplay);
+            }
+        });
+    }
 }
 
-function createNotification(strToDisplay, timerInMiSeconds){
-    setTimeout(pushNotify(strToDisplay), timerInMiSeconds);
-}
+function delayedNotification(strToDisplay, timerInMiSeconds){
 
+    setTimeout(function(){
+        if (Notification.permission === "granted") {
+            // If it's okay, create a notification
+            var notification = new Notification(strToDisplay);
+        }
+        // Otherwise, ask the user for permission
+        else if (Notification.permission !== 'denied') {
+            Notification.requestPermission(function (permission) {
+                // If the user accepts, create a notification
+                if (permission === "granted") {
+                    var notification = new Notification(strToDisplay);
+                }
+            });
+        }
+    }, timerInMiSeconds);
+}
